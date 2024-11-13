1b997b142c8eb7412b9c867d4c1a86dc0fbd4e79
chloe <chloep2838@tuckahoeschools.org>
2024-11-13 13:30:47 +0000

    fourth commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const question = document.querySelector('.question-container')
+question.textContent = "answerList[0]"
+
+const questions = document.getElementById("question-container");
+const questionList = ["Question 1", "Question 2", "Question 3"];
+
+let score = 0;
+
+let currentQuestionIndex = 0
+
