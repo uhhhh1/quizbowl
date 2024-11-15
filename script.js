7aa0a6e353c25abf922af2b91180e51a10262c30
chloe <chloep2838@tuckahoeschools.org>
2024-11-15 13:33:04 +0000

    sixth commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let questionList = ['what is skibidi toilet', 'who is skibidi toilet', 'why is skibidi toilet'];
-let answerList = ['skibidi', 'skibidi', 'skibidi'];
+let questionList = ['what is the bro version of huzz', 'who are those who knkow', 'is lebron james my sunshine', 'What is called when you look better than someone?', 'Put the still water down!', 'who is big sister general'];
+let answerList = ['bruzz', 'balkans', 'yes', 'mogging', 'this is for those who know', 'Kamala Harris'];
 let score = 0;
 let questionIndex = 0;
 
 const question = document.querySelector('.question-container')
-const button = document.querySelector('.question-container')
+const button = document.querySelector('.submit-button')
 const input = document.querySelector('#input-feild')
 let scoreContainer = document.querySelector('.score-container')
 
@@ -12,6 +12,7 @@ question.textContent = questionList[questionIndex]
 
 let SubmitFunction = () =>{
     button.addEventListener('click', () =>{
+        console.log("Button clicked");
         if(input.value === answerList[questionIndex]){
             questionIndex += 1;
             score += 1;
@@ -21,14 +22,17 @@ let SubmitFunction = () =>{
             score -= 1;
             scoreContainer.textContent = "Score: " + score;
         }
-        //PUT ENDGAME FUNCTION HERE
     });
 }
 
 let endGame = () =>{
-    if(questionIndex > 2){
+    if(questionIndex > 5){
         scoreContainer.textContent = "Final Score: " + score;
-        question.textContent = "Congradulations, play again!";
-
+        question.textContent = "6/6 your fried vro";
+        input.style.display = "none";
+        button.style.display = "none";
     }
 }
+
+
+SubmitFunction();
