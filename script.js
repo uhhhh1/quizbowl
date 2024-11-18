6409bd79d2f3cfd59b889c5863a6b639e9916234
chloe <chloep2838@tuckahoeschools.org>
2024-11-18 01:27:23 +0000

    finished the javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let questionList = ['what is the bro version of huzz', 'who are those who knkow', 'is lebron james my sunshine', 'What is called when you look better than someone?', 'Put the still water down!', 'who is big sister general'];
-let answerList = ['bruzz', 'balkans', 'yes', 'mogging', 'this is for those who know', 'Kamala Harris'];
+let answerList = ['bruzz', 'balkans', 'yes', 'mogging', 'this is for those who know', 'kamala harris'];
 let score = 0;
 let questionIndex = 0;
 
@@ -22,13 +22,15 @@ let SubmitFunction = () =>{
             score -= 1;
             scoreContainer.textContent = "Score: " + score;
         }
+        endGame();
+
     });
 }
 
 let endGame = () =>{
     if(questionIndex > 5){
         scoreContainer.textContent = "Final Score: " + score;
-        question.textContent = "6/6 your fried vro";
+        question.textContent = "6/6 im fried vro";
         input.style.display = "none";
         button.style.display = "none";
     }
